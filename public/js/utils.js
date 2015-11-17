d2e80a35eaab0b53dd4c60517692fb90de833574
Joey Bronner <joeybronner@gmail.com>
2015-11-17 18:20:39 +0100

    SVG/Utils files

---
+++ public/js/utils.js
@@ -0,0 +1,57 @@
+function searchUser(e) {
+    if (e.keyCode == 13) {
+    	document.getElementById('canvas').innerHTML = '<rect id="background-rect" x="0%" y="0%" width="100%" height="100%" fill="#F5F5F5"/>';
+        doload(document.getElementById("fixed-header-drawer-exp").value)
+        return false;
+    }
+}
+
+function changeSection(section) {
+	var sections = ['myboard', 'friends', 'random', 'customize'];
+	for (var s in sections) {
+	  document.getElementById(sections[s]).className = document.getElementById(sections[s]).className.replace(' is-active', '');
+	}
+	// Show requested view
+	document.getElementById(section).className += ' is-active';
+
+	switch(section) {
+	    case 'myboard':
+	        // TODO: Call good function
+	        break;
+	    case 'friends':
+	        // TODO: Call good function
+	        break;
+	    case 'random':
+	    	// TODO: Call good function
+	    	break;
+	    case 'customize':
+	    	loadApparencePanel();
+	    	break;
+	}
+
+	// Hide left panel
+	document.getElementById('left-panel').className = document.getElementById('left-panel').className.replace(' is-visible', '');
+}
+
+function loadApparencePanel() {
+	// Load user data
+	var userData = getJSONFile(authentifiedUser);
+
+	if (userData != null) {
+		// Display his values
+		document.getElementById('backgroundcolor').value = userData.apparences[0].backgroundcolor;
+	}
+}
+
+function getJSONFile(user) {
+	var d = null;
+    $.ajax({
+        url: 'data/dataset_' + user + '.json',
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            d = data;
+        }
+    });
+    return d;
+}
