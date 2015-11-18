06190928dcccf5c1eceda9dfc9bd7377dd07e4de
Joey Bronner <joeybronner@gmail.com>
2015-11-18 16:12:58 +0100

    Remove ignored files

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//url : 'mongodb://<user>:<pass>@mongo.onmodulus.net:27017/uw45mypu'
-	//url : 'mongodb://localhost:27017/moodboard_database'
+	//url : 'mongodb://localhost:27017/moodboard_database' 
 }
