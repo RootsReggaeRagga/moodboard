bd44b622bcc8f8c9dc60b28652ba6f9d6d52537d
Joey Bronner <joeybronner@gmail.com>
2016-01-17 16:22:00 +0100

    Cleaning Utilitaire JS class

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,64 +1,10 @@
-function searchUser(e) {
-    if (e.keyCode == 13) {
-    	document.getElementById('canvas').innerHTML = '<rect id="background-rect" x="0%" y="0%" width="100%" height="100%" fill="#F5F5F5"/>';
-        doload(document.getElementById("fixed-header-drawer-exp").value)
-        return false;
-    }
-}
-/*
-function changeSection(section) {
-	var sections = ['myboard', 'friends', 'random', 'customize'];
-	for (var s in sections) {
-	  document.getElementById(sections[s]).className = document.getElementById(sections[s]).className.replace(' is-active', '');
-	}
-	// Show requested view
-	document.getElementById(section).className += ' is-active';
-
-	switch(section) {
-	    case 'myboard':
-	        // TODO: Call good function
-	        break;
-	    case 'friends':
-	        // TODO: Call good function
-	        break;
-	    case 'random':
-	    	// TODO: Call good function
-	    	break;
-	    case 'customize':
-	    	loadApparencePanel();
-	    	break;
-	}
-
-	// Hide left panel
-	document.getElementById('left-panel').className = document.getElementById('left-panel').className.replace(' is-visible', '');
-}*/
-
-function loadApparencePanel() {
-	// Load user data
-	var userData = getJSONFile(authentifiedUser);
-
-	if (userData != null) {
-		// Display his values
-		document.getElementById('backgroundcolor').value = palette.get(userData.apparences[0].colorpalette, '100');
-	}
-}
-
-function getJSONFile(user) {
-	var d = null;
-    $.ajax({
-        url: 'data/dataset_' + user + '.json',
-        dataType: 'json',
-        async: false,
-        success: function(data) {
-            d = data;
-        }
-    });
-    return d;
-}
-
 function isHexaColor(sNum) {
 	var color = sNum.substring(1, sNum.length);
   	return (typeof color === "string") && color.length === 6 && ! isNaN(parseInt(color, 16));
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
 function applyUserInterfaceStyle(color) {
@@ -70,10 +16,6 @@ function applyUserInterfaceStyle(color) {
 	if (backgroundRect !== null) {
 		backgroundRect.setAttribute("fill", palette.get(color, '100'));
 	}
-}
-
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
 function capitalizeFirstLetter(string) {
@@ -88,8 +30,4 @@ function eventFire(el, etype){
     evObj.initEvent(etype, true, false);
     el.dispatchEvent(evObj);
   }
-}
-
-function showALert() {
-	console.log('alerrrt');
 }
