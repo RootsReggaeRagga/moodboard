15fc6ed32318ef24ffd37ef7ff21b1860084d407
Joey Bronner <joeybronner@gmail.com>
2016-02-06 19:34:18 +0100

    New toast on logging page

--- public/controllers/authentication.client.controller.js
+++ public/controllers/authentication.client.controller.js
@@ -1,17 +1,49 @@
 'use strict';
 
-angular.module('mean').controller('AuthenticationController', ['$scope', '$rootScope', '$http', '$location', 'Authentication', 'SandboxSrv', 'Menu',
-    function($scope, $rootScope, $http, $location, Authentication, SandboxSrv, Menu) {
+angular.module('mean').controller('AuthenticationController', ['$scope', '$rootScope', '$http', '$location', 'Authentication', 'SandboxSrv', '$mdToast',
+    function($scope, $rootScope, $http, $location, Authentication, SandboxSrv, $mdToast) {
         $scope.authentication = Authentication;
 
-        //If user is signed in then redirect back home
+        // If user is signed in then redirect back home
         if ($scope.authentication.user) {
             $location.path('/');
-        } else {
-            Menu.updateMenu(false, 'username');
         }
 
-        var snackbarContainer = document.querySelector('#toast');
+        // Toast
+        var last = {
+            bottom: false,
+            top: true,
+            left: false,
+            right: true
+        };
+
+        $scope.toastPosition = angular.extend({},last);
+        
+        $scope.getToastPosition = function() {
+            sanitizePosition();
+            return Object.keys($scope.toastPosition)
+                .filter(function(pos) { return $scope.toastPosition[pos]; })
+                .join(' ');
+        };
+
+        function sanitizePosition() {
+            var current = $scope.toastPosition;
+            if ( current.bottom && last.top ) current.top = false;
+            if ( current.top && last.bottom ) current.bottom = false;
+            if ( current.right && last.left ) current.left = false;
+            if ( current.left && last.right ) current.right = false;
+            last = angular.extend({},current);
+        }  
+
+        $scope.showSimpleToast = function(msg) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent(msg)
+                    .position($scope.getToastPosition())
+                    .hideDelay(3000)
+                );
+        };
+        // End Toast
 
         $scope.signup = function() {
             $http.post('/auth/signup', $scope.credentials).success(function(response) {
@@ -45,8 +77,8 @@ angular.module('mean').controller('AuthenticationController', ['$scope', '$rootS
                 var data = {
                     message: $scope.error
                 };
-                snackbarContainer.MaterialSnackbar.showSnackbar(data);
+                $scope.showSimpleToast(data.message);
             });
         };
     }
-]);
+    ]);
